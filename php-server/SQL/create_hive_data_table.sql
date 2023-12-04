48c7c74a31a8430295c3a36546d8bacf22d8db13
YumYum <yumethsumathipala@gmail.com>
2023-12-04 19:16:24 +0530

    Format sql files

--- php-server/SQL/create_hive_data_table.sql
+++ php-server/SQL/create_hive_data_table.sql
@@ -1,8 +1,9 @@
-CREATE TABLE hive_data (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    unit_id INT NOT NULL,
-    temp DOUBLE NOT NULL,
-    hum DOUBLE NOT NULL,
-    weight DOUBLE NOT NULL,
+CREATE TABLE hive_data
+(
+    id         INT AUTO_INCREMENT PRIMARY KEY,
+    unit_id    INT    NOT NULL,
+    temp       DOUBLE NOT NULL,
+    hum        DOUBLE NOT NULL,
+    weight     DOUBLE NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
--- php-server/SQL/create_hive_units_table.sql
+++ php-server/SQL/create_hive_units_table.sql
@@ -1,6 +1,7 @@
-CREATE TABLE hive_units (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    api_key CHAR(25) NOT NULL UNIQUE,
+CREATE TABLE hive_units
+(
+    id          INT AUTO_INCREMENT PRIMARY KEY,
+    api_key     CHAR(25) NOT NULL UNIQUE,
     description TEXT,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    created_at  TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
