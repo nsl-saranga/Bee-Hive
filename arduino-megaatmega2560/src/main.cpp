9ac03bde7a3171f929262279814a0afcab7612bc
nsl-web <finn.collins129@gmail.com>
2023-12-11 23:18:49 +0530

    added the GPRS shield code

--- arduino-megaatmega2560/src/main.cpp
+++ arduino-megaatmega2560/src/main.cpp
@@ -9,7 +9,7 @@
 #include <DHT_U.h>
 #include <HX711.h>
 #include <arduinoFFT.h>
-
+#include <SoftwareSerial.h>
 #include <ArduinoJson.h>
 
 #define DHTTYPE DHT22
@@ -33,9 +33,9 @@ arduinoFFT fft = arduinoFFT();
 #define SAMPLING_FREQUENCY 10000  // Hz, must be less than 10000 due to ADC limitations
 
 // Defines the interval (in milliseconds) at which you want to process the accumulated data.
-#define Prcoess_INTERVAL 5000  // 5 seconds in milliseconds
+#define Prcoess_INTERVAL 5*60*1000  // 5 mins in milliseconds
 // Defines the interval (in milliseconds) at which you want to sample data (call perform_FFT()).
-#define FFT_SAMPLING_INTERVAL 1000  // 1 second in milliseconds
+#define FFT_SAMPLING_INTERVAL 1*60*1000  // 1 min in milliseconds
 
 //  Keeps track of the time (in milliseconds) when the last FFT processing occurred.
 unsigned long lastProcessTime = 0; //  Keeps track of the time (in milliseconds) when the last FFT processing occurred.
@@ -52,8 +52,7 @@ double vImag[SAMPLES];
 
 // GSM auth details
 // `GSM_APN, GSM_USER, GSM_PASSWORD` are defined
-
-
+SoftwareSerial SIM900(7, 8);//Rx,TX
 
 void setup()
 {
@@ -62,7 +61,9 @@ void setup()
   scale.begin(LOADCELL_DO_PIN, LOADCELL_CK_PIN);
   scale.set_scale(0); // <<<<<<<<<<<<<<<<<< replace with the "Calibration factor" value
   scale.tare();
-
+  SIM900.begin(9600);
+  // Initialize GPRS connection
+  initializeGPRS();
 }
 
 void loop()
@@ -98,7 +99,25 @@ void loop()
     float temp = dht.readTemperature();
     float humidity = dht.readHumidity();
     float weight = scale.get_units(10);
-    accumulatedData;
+    float frequencies[5] = {accumulatedData[0], accumulatedData[1], accumulatedData[2], accumulatedData[3], accumulatedData[4]};
+    // Create JSON payload
+    String jsonPayload = "{\"temperature\":" + String(temp) +
+                        ",\"humidity\":" + String(humidity) +
+                        ",\"weight\":" + String(weight) +
+                        ",\"frequencies\":[";
+                        
+    // Add frequencies to the JSON payload
+    for (int i = 0; i < 5; ++i) {
+        jsonPayload += String(frequencies[i]);
+        if (i < 4) {
+            jsonPayload += ","; // Add a comma except for the last element
+        }
+    }
+    jsonPayload += "]}";
+
+    // Send data to the API
+    sendDataToAPI(jsonPayload);
+
     if(dataIndex>=5)
     {
       memset(accumulatedData, 0, sizeof(accumulatedData));
@@ -111,13 +130,6 @@ void loop()
     // Update the last FFT time
     lastProcessTime = currentTime;
   }
-
-  // The rest of your code can go here, outside the FFT and sampling intervals
-  // ...
-
-  // Other tasks or delays can be added here if needed
-  // ...
-  // keep the loop delay up here so there is a delay before the first iteration.
 }
 
 float perform_FFT(){
@@ -131,6 +143,7 @@ float perform_FFT(){
   double maxMagnitude = 0.0;
   
   for (int i = 1; i < SAMPLES / 2; i++) {
+    // int mic = analogRead(microphonePin);
     if (vReal[i] > maxMagnitude) {
       maxMagnitude = vReal[i];
       peakIndex = i;
@@ -140,6 +153,63 @@ float perform_FFT(){
   // Calculate the frequency corresponding to the peak index
   float frequency = peakIndex * (SAMPLING_FREQUENCY / 2.0) / SAMPLES;
   return frequency;
+}
+
+void initializeGPRS() {
+  SIM900.println("AT");
+  delay(1000);
+  SIM900.println("AT+CPIN?");
+  delay(1000);
+  SIM900.println("AT+CREG?");
+  delay(1000);
+  SIM900.println("AT+CGATT?");
+  delay(1000);
+  SIM900.println("AT+CIPSHUT");
+  delay(1000);
+  SIM900.println("AT+CIPSTATUS");
+  delay(1000);
+  SIM900.println("AT+CIPMUX=0");
+  delay(1000);
+  SIM900.println("AT+CSTT=\"GSM_APN\"");
+  delay(1000);
+  SIM900.println("AT+CIICR");
+  delay(3000);
+  SIM900.println("AT+CIFSR");
+  delay(2000);
+}
+
+void sendDataToAPI(String data) {
+  SIM900.println("AT+CIPSTART=\"TCP\",\"" + String(API_SERVER) + "\",80");
+  delay(2000);
+  if (SIM900.find("OK")) {
+    Serial.println("TCP Connection Established");
+  } else {
+    Serial.println("Error: Unable to establish TCP connection");
+    return;
+  }
+
+  String postRequest =
+      "POST " + String(API_PATH) + " HTTP/1.1\r\n" +
+      "Host: " + String(API_SERVER) + "\r\n" +
+      "Content-Type: application/json\r\n" +
+      "Content-Length: " + String(data.length() + 2) + "\r\n" +
+      "Authorization: Basic " + String(GSM_APN) + "\r\n" +
+      "\r\n" +
+      data;
+
+  SIM900.println("AT+CIPSEND=" + String(postRequest.length()));
+  delay(1000);
+  if (SIM900.find(">")) {
+    Serial.println("Sending data...");
+    SIM900.print(postRequest);
+  } else {
+    Serial.println("Error: Unable to send data");
+  }
+
+  delay(5000); // Wait for the server response (adjust accordingly)
+
+  // Close the connection
+  SIM900.println("AT+CIPCLOSE");
 }
 #endif
 
